55286b147f5c2b6928ecc56b3f57d5d3715d52f8
halil cosgun <cosgun.halil@gmail.com>
2018-09-07 07:58:10 +0300

    Basic buttons and functionality added
    button1,
    button2,

---
+++ LB_InputManager/Assets/Scripts/InputTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTest : MonoBehaviour {
+
+    private void Start()
+    {
+        LB_InputManager.Instance.InputGettedEvent += TouchDown;
+    }
+
+    public void TouchDown(LB_InputInfo info)
+    {
+        Debug.Log(info.button);
+        Debug.Log(info.inputType);
+        Debug.Log(info.info);
+    }
+}
---
+++ LB_InputManager/Assets/Scripts/LB_InputInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LB_InputInfo {
+
+    public InputType inputType;
+    public ButtonType button;
+    public object[] info;
+
+    public LB_InputInfo(ButtonType button, InputType inputType, object[] info = null)
+    {
+        this.button = button;
+        this.inputType = inputType;
+        this.info = info;
+    }
+}
